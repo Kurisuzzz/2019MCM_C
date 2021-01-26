da9ac8ee17eb9bb34880326969f30927e38986cf
Kurisuzzz <984459260@qq.com>
2021-01-26 12:00:03 +0800

    add pictures for prediction

--- code/WritePicture.py
+++ code/WritePicture.py
@@ -6,7 +6,9 @@ import openpyxl
 PA_Data = pd.read_excel(r"F:\360MoveData\Users\Dell\Desktop\2019MCM_C\2019MCM_C\data\2018_MCMProblemC_DATA\PA.xlsx")
 year = set(PA_Data['YYYY'].values)
 
+# 得到每年的数据
 for i in year:
   test = PA_Data.loc[PA_Data['YYYY'] == i]
   test.to_csv(r'F:\360MoveData\Users\Dell\Desktop\2019MCM_C\2019MCM_C\data\2018_MCMProblemC_DATA\PA_{0}.csv'.format(i))
 
+# 得到每个县的数据
